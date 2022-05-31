d5c3756ddd06791c64c2c36bb7cccc12f3431db1
dbonafin <demianbonafina@gmail.com>
2022-05-31 17:00:42 +0200

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+// 1 - DATA COLLECT
+const userFirstName = prompt(`Scrivi il tuo nome`);
+const userLastName = prompt(`Scrivi il tuo cognome`);
+const userFavColour = prompt(`Scrivi il tuo colore preferito`);
+
+// 2 - LOGIC PROCESS OF THE RESULT
+// construction of the password result
+const pwdResult = userFirstName + userLastName + userFavColour + 21;
+// construction of the final output 
+const userMessage = `La tua password è ` + pwdResult;
+
+// 3 - OUTPUT RESULT
+document.getElementById (`password-result`).innerHTML = userMessage;
